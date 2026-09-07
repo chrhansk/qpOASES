be2a013344ea8a827b09e88973572f37ef5d6e96
Christoph Hansknecht <christoph.hansknecht@tu-clausthal.de>
2026-09-07 10:43:28 +0200

    Fix bug in error handling

--- interfaces/python/qpoases.pyx
+++ interfaces/python/qpoases.pyx
@@ -254,8 +254,8 @@ cdef class PyReturnValue:
 
 
 class QPOASESError(Exception):
-    def __init__(self, PyReturnValue error_code):
-        self.error_code = error_code
+    def __init__(self, error_code):
+        self.error_code = PyReturnValue(error_code)
         message = f"QPOASES error {str(error_code)}"
         super().__init__(message)
 
