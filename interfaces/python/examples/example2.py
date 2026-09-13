39b1dc31baaa2e7b41a90c2a3d3c7a2cc8b67b60
Christoph Hansknecht <christoph.hansknecht@tu-clausthal.de>
2026-09-13 20:43:14 +0200

    Fix shapes in examples

--- interfaces/python/examples/example2.py
+++ interfaces/python/examples/example2.py
@@ -32,7 +32,7 @@ from qpoases import PySolutionAnalysis as SolutionAnalysis
 
 #  Setup data of first QP.
 H = np.array([ 1.0, 0.0, 0.0, 0.5 ]).reshape((2,2))
-A = np.array([ 1.0, 1.0 ]).reshape((2,1))
+A = np.array([ 1.0, 1.0 ]).reshape((1,2))
 g = np.array([ 1.5, 1.0 ])
 lb = np.array([ 0.5, -2.0 ])
 ub = np.array([ 5.0, 2.0 ])
@@ -41,7 +41,7 @@ ubA = np.array([ 2.0 ])
 
 #  Setup data of second QP.
 H_new = np.array([ 1.0, 0.5, 0.5, 0.5 ]).reshape((2,2))
-A_new = np.array([ 1.0, 5.0 ]).reshape((2,1))
+A_new = np.array([ 1.0, 5.0 ]).reshape((1,2))
 g_new = np.array([ 1.0, 1.5 ])
 lb_new = np.array([ 0.0, -1.0 ])
 ub_new = np.array([ 5.0, -0.5 ])
